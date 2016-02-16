dcbd45cbfe0891dc1622b93749dbf0b15a1613e4
Kai Wang <kai.wang.nankai@gmail.com>
2016-02-16 18:16:34 +1100

    third commit

---
+++ 2.2.10 Copy List with Random Pointer.py
@@ -0,0 +1,46 @@
+class RandomListNode:
+	def __init__(self, val, next = None):
+		self.val = val
+		self.next, self.random = next, None
+
+class Solution:
+	def copyRandomList(self, head):
+		node = head
+		while node:
+			next = node.next
+			node.next = RandomListNode(node.val)
+			node.next.next = next
+			node = next
+		node = head
+		while node and node.next:
+			if node.random:
+				node.next.random = node.random.next
+			node = node.next.next
+		node = head
+		copy = head.next
+		while node:
+			tmp = node.next
+			node.next = tmp.next
+			node = node.next
+			if node:
+				tmp.next = node.next
+		return copy
+
+head = RandomListNode(0, RandomListNode(1, RandomListNode(2, RandomListNode(3))))
+head.random = head.next.next
+head.next.random = head
+head.next.next.random = head.next
+
+inst = Solution()
+copy = inst.copyRandomList(head)
+
+print(copy.random.val)
+print(copy.next.random.val)
+print(copy.next.next.random.val)
+print(copy.next.next.next.random == None)
+
+while copy:
+	print(copy.val, end = ' ')
+	copy = copy.next
+print()
+
---
+++ 2.2.9 Reverse Nodes in k-Group.py
@@ -0,0 +1,51 @@
+class ListNode:
+	def __init__(self, val, next = None):
+		self.val = val
+		self.next = next
+
+class Solution:
+	def reverseKGroup(self, head, k):
+		dummy = ListNode(0, head)
+		prevT = dummy
+		cur = head
+		
+		while cur:
+			head = cur
+			counter = 0
+			while cur and counter < k - 1:
+				cur = cur.next
+				counter += 1
+			if cur:
+				next = cur.next
+				cur.next = None
+				H, T = self.reverse(head)
+				prevT.next = H
+				T.next = next
+				prevT = T
+				cur = next
+			else:
+				return dummy.next
+		return dummy.next
+
+
+	def reverse(self, head):
+		prev = None
+		cur = head
+		while cur:
+			next = cur.next
+			cur.next = prev
+			prev = cur
+			cur = next
+		return prev, head
+
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6, ListNode(7, ListNode(8, ListNode(9)))))))))
+head = ListNode(0)
+head = None
+
+inst = Solution()
+head = inst.reverseKGroup(head, 4)
+
+while head:
+	print(head.val, end = ' ')
+	head = head.next
+print()
