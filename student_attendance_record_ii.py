0fbd09c40d262d4f3c9e84439e561cc8eb235d5c
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-21 16:56:43 +0800

    Updated student_attendance_record_ii question
    Optimised memory

--- student_attendance_record_ii.py
+++ student_attendance_record_ii.py
@@ -1,27 +1,15 @@
-import itertools
-
-
 class Solution(object):
     def check_record(self, n):
-        return self.calc_f_seq(n) % (10 ** 9 + 7)
-
-    def calc_f_seq(self, n):
-        base_val = [1, 3, 8, 19]
+        seq = [1, 3, 8, 19]
         if n <= 3:
-            return base_val[n]
-        a_seq = self.calc_a_seq(n - 1)
-        counter = 0
-        while counter + 3 < n:
-            base_val.append(
-                base_val[-1] * 2 - base_val[-4] +
-                a_seq[counter + 3] - a_seq[counter])
-            counter += 1
-            base_val = base_val[1:]
-        return base_val[-1]
-
-    def calc_a_seq(self, n):
-        base_val = [1, 2, 4, 7]
+            return seq[n]
+        lp_only_seq = [1, 2, 4, 7]
         while n > 3:
-            base_val.append(base_val[-1] * 2 - base_val[-4])
+            seq.append(
+                seq[-1] * 2 - seq[-4] +
+                lp_only_seq[-1] - lp_only_seq[-4])
+            seq.pop(0)
             n -= 1
-        return base_val
+            lp_only_seq.append(lp_only_seq[-1] * 2 - lp_only_seq[-4])
+            lp_only_seq.pop(0)
+        return seq[-1] % (10 ** 9 + 7)
--- student_attendance_record_ii_test.py
+++ student_attendance_record_ii_test.py
@@ -1,4 +1,3 @@
-import itertools
 from unittest import TestCase
 
 from student_attendance_record_ii import Solution
@@ -8,22 +7,10 @@ class TestCheckRecord(TestCase):
     def test_check_record(self):
         # Setup
         sol = Solution()
-        n = 100000
+        n = 99999
 
         # Exercise
         ans = sol.check_record(n)
 
         # Verify
-        self.assertEqual(ans, 749184020)
-
-    def test_calc_a_seq(self):
-        # Setup
-        sol = Solution()
-
-        # Exercise
-        ans = sol.calc_a_seq(20)
-
-        # Verify
-        self.assertEqual(ans[-1], 223317)
-
-
+        self.assertEqual(ans, 816671055)
