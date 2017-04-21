4996e1b395ac036bc43fabdf39bf6e936cfe28e4
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-21 20:02:30 +0800

    Updated student_attendance_record_ii question
    - Optimised algorithm
    - Added analysis

--- student_attendance_record_ii.py
+++ student_attendance_record_ii.py
@@ -1,15 +1,65 @@
 class Solution(object):
     def check_record(self, n):
+        """
+        How to form n X...X from (n - 1) X...X
+
+        * append P
+            Any sequence
+
+            => seq(n - 1)
+
+        * append A
+            Any sequence without A
+            lp_seq(n) = lp_seq(n - 1) + lp_seq(n - 2) + lp_seq(n - 3)
+                * (n - 1) X...X ends with P
+                * (n - 1) X...X ends with PL
+                * (n - 2) X...X ends with PLL
+            lp_seq(0) = 1
+            lp_seq(1) = 2
+            lp_seq(2) = 4
+            lp_seq(3) = 7
+
+            => lp_seq(n - 1)
+
+        * append L
+            Any sequence not ends with LL
+                (n - 4) X...X + P + LL
+                    seq(n - 4)
+                (n - 4) X...X + A + LL
+                    lp_seq(n - 4)
+
+            => seq(n - 1) - seq(n - 4) - lp_seq(n - 4)
+
+
+        seq(n) = 2 * seq(n - 1) - seq(n - 4) + lp_seq(n - 1) - lp_seq(n - 4)
+
+        seq(0) = 1
+        seq(0) = 3
+        seq(0) = 8
+        seq(0) = 19
+
+        """
+        # Base sequence to start with
         seq = [1, 3, 8, 19]
+
+        # Simple case
         if n <= 3:
             return seq[n]
-        lp_only_seq = [1, 2, 4, 7]
-        while n > 3:
-            seq.append(
-                seq[-1] * 2 - seq[-4] +
-                lp_only_seq[-1] - lp_only_seq[-4])
-            seq.pop(0)
-            n -= 1
-            lp_only_seq.append(lp_only_seq[-1] * 2 - lp_only_seq[-4])
-            lp_only_seq.pop(0)
-        return seq[-1] % (10 ** 9 + 7)
+
+        # Modulo base
+        M = 10 ** 9 + 7
+
+        # Calculate sequence without A (with only L and P)
+        m = n - 1
+        lp_seq = [1, 2, 4, 7]
+        while m > 3:
+            lp_seq.append(sum(lp_seq[-3:]) % M)
+            m -= 1
+
+        # Calculate sequence
+        counter = 0
+        while counter + 3 < n:
+            seq.append((seq[-1] * 2 - seq[-4] +
+                        lp_seq[counter + 1] + lp_seq[counter + 2]) % M)
+            counter += 1
+        return seq[-1]
--- student_attendance_record_ii_test.py
+++ student_attendance_record_ii_test.py
@@ -1,16 +1,31 @@
+import random
 from unittest import TestCase
 
 from student_attendance_record_ii import Solution
 
 
 class TestCheckRecord(TestCase):
+
+    def check_record(self, n):
+        seq = [1, 3, 8, 19]
+        if n <= 3:
+            return seq[n]
+        lp_seq = [1, 2, 4, 7]
+        M = 10 ** 9 + 7
+        while n > 3:
+            seq.append((seq[-1] * 2 - seq[-4] + sum(lp_seq[-3:-1])) % M)
+            n -= 1
+            lp_seq.append(sum(lp_seq[-3:]) % M)
+        return seq[-1] % M
+
     def test_check_record(self):
         # Setup
         sol = Solution()
-        n = 99999
+        n = random.randint(1, 100000)
 
         # Exercise
         ans = sol.check_record(n)
+        expected_ans = self.check_record(n)
 
         # Verify
-        self.assertEqual(ans, 816671055)
+        self.assertEqual(ans, expected_ans)
