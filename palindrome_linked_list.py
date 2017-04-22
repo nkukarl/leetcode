bf1b004f6cb96280b44e41697d44fd5eced4f2a8
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-22 14:26:49 +0800

    Added palindrome_linked_list question

---
+++ palindrome_linked_list.py
@@ -0,0 +1,110 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def is_palindrome(self, head):
+        """
+
+        Args:
+            head (ListNode): head of linked list
+
+        Returns:
+            bool
+
+        """
+        length = self.get_length(head)
+        if length in [0, 1]:
+            return True
+        if 2 == length:
+            return head.val == head.next.val
+
+        mid = self.split(head, length)
+        tail = self.reverse(mid)
+        return self.compare(head, tail)
+
+    def get_length(self, head):
+        """
+
+        Args:
+            head (ListNode): head of linked list
+
+        Returns:
+            length of linked list
+
+        """
+        node = head
+        length = 0
+        while node is not None:
+            length += 1
+            node = node.next
+        return length
+
+    def split(self, head, length):
+        """
+
+        Args:
+            head (ListNode): head of linked list
+            length (int): length of linked list
+
+        Returns:
+            head, mid point
+
+        """
+        n = length // 2
+        node = head
+        prev = None
+        while n:
+            prev = node
+            node = node.next
+            n -= 1
+        mid = node
+        if 1 == length % 2:
+            # If length is an odd number, skip the true mid point
+            # and move to the next
+            mid = mid.next
+        prev.next = None
+        return mid
+
+    def reverse(self, head):
+        """
+
+        Args:
+            head (ListNode): head of linked list
+
+        Returns:
+            tail
+
+        """
+        node = head
+        prev = None
+        while node is not None:
+            next_ = node.next
+            node.next = prev
+            prev = node
+            node = next_
+        return prev
+
+    def compare(self, head1, head2):
+        """
+
+        Args:
+            head1 (ListNode): head of linked list
+            head2 (ListNode): head of linked list
+
+        Returns:
+            bool
+
+        """
+        if head1 is None and head2 is None:
+            return True
+        node1, node2 = head1, head2
+        while node1 is not None and node2 is not None:
+            if node1.val != node2.val:
+                return False
+            node1 = node1.next
+            node2 = node2.next
+        if node1 is not None or node2 is not None:
+            return False
+        return True
---
+++ palindrome_linked_list_test.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from palindrome_linked_list import ListNode, Solution
+
+
+class TestPalindromeLinkedList(TestCase):
+    def test_is_palindrome(self):
+        # Setup
+        sol = Solution()
+        # linked_list_raw = [1, 2, 3, 4, 3, 2, 1]
+        # linked_list_raw = [1, 2, 3, 4, 4, 3, 2, 1]
+        linked_list_raw = [1, 2, 3, 4, 4, 2, 1]
+        head = self.get_head(linked_list_raw)
+
+        # Exercise
+        ans = sol.is_palindrome(head)
+        expected_ans = self.is_palindrome(linked_list_raw)
+
+        # Verify
+        self.assertEqual(ans, expected_ans)
+
+    def get_head(self, linked_list_raw):
+        if 0 == len(linked_list_raw):
+            return None
+        head = ListNode(linked_list_raw[0])
+        node = head
+        for val in linked_list_raw[1:]:
+            node.next = ListNode(val)
+            node = node.next
+        return head
+
+    def is_palindrome(self, linked_list_raw):
+        left, right = 0, len(linked_list_raw) - 1
+        while left < right:
+            if linked_list_raw[left] != linked_list_raw[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
