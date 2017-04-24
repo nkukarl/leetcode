af7f72d8fab6b1208ff74f63484c194ebb5e0a40
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-24 17:44:23 +0800

    Updated tree utility methods

--- utils_tree.py
+++ utils_tree.py
@@ -59,5 +59,7 @@ def get_root_tree(tree_raw):
 def compare_trees(root1, root2):
     if root1 is None and root2 is None:
         return True
+    if root1 is None or root2 is None or root1.val != root2.val:
+        return False
     return compare_trees(root1.left, root2.left) and \
            compare_trees(root1.right, root2.right)
