15902b05539ee634b06980ce3df9e6c9de9fdb13
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-24 17:40:10 +0800

    Updated single_number question

--- single_number.py
+++ single_number.py
@@ -1,6 +1,25 @@
 class Solution:
-    def single_number(self, numbers):
-        mark = 0
+    def single_number_i(self, numbers):
+        return self._get_xor(numbers)
+
+    def single_number_iii(self, numbers):
+        mark_raw = self._get_xor(numbers)
+        mark = 1
+        while mark_raw != 0:
+            if 1 == mark_raw & 1:
+                break
+            mark <<= 1
+            mark_raw >>= 1
+        seq_a, seq_b = [], []
         for n in numbers:
-            mark ^= n
-        return mark
+            if 0 == n & mark:
+                seq_a.append(n)
+            else:
+                seq_b.append(n)
+        return [self._get_xor(seq_a), self._get_xor(seq_b)]
+
+    def _get_xor(self, numbers):
+        xor = 0
+        for n in numbers:
+            xor ^= n
+        return xor
--- single_number_test.py
+++ single_number_test.py
@@ -5,7 +5,7 @@ from single_number import Solution
 
 
 class TestSingleNumber(TestCase):
-    def test_single_number(self):
+    def test_single_number_i(self):
         # Setup
         sol = Solution()
         unique_number = random.randint(1, 9)
@@ -13,7 +13,22 @@ class TestSingleNumber(TestCase):
         random.shuffle(numbers)
 
         # Exercise
-        ans = sol.single_number(numbers)
+        ans = sol.single_number_i(numbers)
 
         # Verify
         self.assertEqual(ans, unique_number)
+
+    def test_single_number_iii(self):
+        # Setup
+        sol = Solution()
+        M = random.randint(3, 10)
+        N = random.randint(10, 20)
+        unique_number = random.sample(range(1, M), 2)
+        numbers = [x for x in range(M, N)] * 2 + unique_number
+        random.shuffle(numbers)
+
+        # Exercise
+        ans = sol.single_number_iii(numbers)
+
+        # Verify
+        self.assertEqual(sorted(ans), sorted(unique_number))
