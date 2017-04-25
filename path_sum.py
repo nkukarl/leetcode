828c69d90890a63f7621b992034ad2e04f80a2cb
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-25 16:42:48 +0800

    Updated path_sum question

--- path_sum.py
+++ path_sum.py
@@ -1,34 +1,30 @@
 class Solution(object):
     def has_path_sum(self, root, sum_):
-        if root is None:
-            return False
         self.sum_ = sum_
         self.flag = False
         self.traverse_flag(root, 0)
         return self.flag
 
     def traverse_flag(self, root, cur):
+        if root is None:
+            return
         if root.left is None and root.right is None:
             if cur + root.val == self.sum_:
                 self.flag = True
-        if root.left is not None:
-            self.traverse_flag(root.left, cur + root.val)
-        if root.right is not None:
-            self.traverse_flag(root.right, cur + root.val)
+        self.traverse_flag(root.left, cur + root.val)
+        self.traverse_flag(root.right, cur + root.val)
 
     def path_sum(self, root, sum_):
-        if root is None:
-            return []
         self.sum_ = sum_
         self.valid_paths = []
         self.traverse_path(root, [])
         return self.valid_paths
 
     def traverse_path(self, root, cur):
+        if root is None:
+            return
         if root.left is None and root.right is None:
             if sum(cur) + root.val == self.sum_:
                 self.valid_paths.append(cur + [root.val])
-        if root.left is not None:
-            self.traverse_path(root.left, cur + [root.val])
-        if root.right is not None:
-            self.traverse_path(root.right, cur + [root.val])
+        self.traverse_path(root.left, cur + [root.val])
+        self.traverse_path(root.right, cur + [root.val])
