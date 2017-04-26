ccf46ac39b61a664a22abe4f513acdeb4ce50dd3
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-26 22:26:14 +0800

    Updated gray_code question

--- gray_code.py
+++ gray_code.py
@@ -1,10 +1,8 @@
 class Solution:
     def gray_code(self, n):
-        if type(n) != int or n < 1:
-            raise ValueError
-        if 1 == n:
-            return [0, 1]
+        if 0 == n:
+            return [0]
         first_half = self.gray_code(n - 1)
-        second_half = [x + 2 ** (n - 1) for x in reversed(first_half)]
+        second_half = [x + 2 ** (n - 1) for x in first_half[::-1]]
 
         return first_half + second_half
