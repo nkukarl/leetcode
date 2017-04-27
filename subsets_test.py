c98cef6f95c319484d02c5f3c26f6e29131d2b4a
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-27 09:45:50 +0800

    Updated subsets question

--- subsets_test.py
+++ subsets_test.py
@@ -1,22 +1,36 @@
 from unittest import TestCase
 
+from nose_parameterized import parameterized
+
 from subsets import Solution
 
 
 class TestSubsets(TestCase):
-    def test_subsets(self):
+    @parameterized.expand([
+        [
+            [1, 2, 3],
+            [
+                [],
+                [1], [2], [3],
+                [1, 2], [1, 3], [2, 3],
+                [1, 2, 3],
+            ]
+        ],
+        [
+            [1, 2],
+            [
+                [],
+                [1], [2],
+                [1, 2],
+            ]
+        ],
+    ])
+    def test_subsets(self, nums, expected_ans):
         # Setup
         sol = Solution()
-        nums = [1, 2, 3]
 
         # Exercise
         ans = sol.subsets(nums)
-        expected_ans = [
-            [],
-            [1], [2], [3],
-            [1, 2], [1, 3], [2, 3],
-            [1, 2, 3]
-        ]
 
         # Verify
         self.assertEqual(sorted(ans), sorted(expected_ans))
