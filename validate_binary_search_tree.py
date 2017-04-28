80fe01186af318c84aca3c18c3f98d5a480b0a64
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-28 18:51:32 +0800

    Updated validate_binary_search_tree question

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def is_valid_bst(self, root):
-        return self.validate(root, -2 ** 31 - 1, 2 ** 31 + 1)
+        return self.validate(root, -2 ** 31 - 1, 2 ** 31)
 
     def validate(self, root, low, high):
         if root is None:
