cbfdf1ca2d892554a6457be514c273d109183c27
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-29 22:16:19 +0800

    Updated valid_perfect_square question

--- valid_perfect_square.py
+++ valid_perfect_square.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def is_perfect_square(self, num):
-        sqrt_approximate = self.get_sqrt_approximate(num)
+        sqrt_approximate = self.get_sqrt_int(num)
         for shift in [-1, 0, 1]:
             sqrt = sqrt_approximate + shift
             if sqrt * sqrt == num:
                 return True
         return False
 
-    def get_sqrt_approximate(self, num):
+    def get_sqrt_int(self, num):
+        if num < 0:
+            raise ValueError
+        if num < 1:
+            return 0
         guess = num / 2.0
         eps = 10 ** -9
         while True:
--- valid_perfect_square_test.py
+++ valid_perfect_square_test.py
@@ -1,4 +1,3 @@
-import random
 from unittest import TestCase
 
 from nose_parameterized import parameterized
@@ -27,19 +26,35 @@ class TestValidPerfectSquare(TestCase):
 
         # Exercise
         ans = sol.is_perfect_square(num)
-        expected_ans = int(num ** 0.5) ** 2 == num
+        expected_ans = self.is_perfect_square(num)
 
         # Verify
         self.assertEqual(ans, expected_ans)
 
-    def test_get_sqrt_approximate(self):
+    def is_perfect_square(self, num):
+        return int(num ** 0.5) ** 2 == num
+
+    @parameterized.expand([
+        [
+            0,
+        ],
+        [
+            1,
+        ],
+        [
+            3,
+        ],
+        [
+            13,
+        ],
+    ])
+    def test_get_sqrt_approximate(self, num):
         # Setup
         sol = Solution()
-        num = random.randint(1, 100000)
 
         # Exercise
-        ans = sol.get_sqrt_approximate(num)
-        expected_ans = num ** 0.5
+        ans = sol.get_sqrt_int(num)
+        expected_ans = int(num ** 0.5)
 
         # Verify
-        self.assertEqual(ans, int(expected_ans))
+        self.assertEqual(ans, expected_ans)
