b02a10133777579cf553d61abb13e69fac8cccfb
Kai Wang <kai.wang.nankai@gmail.com>
2017-04-29 23:35:37 +0800

    Updated three_sum question

--- three_sum.py
+++ three_sum.py
@@ -1,18 +1,25 @@
 class Solution:
     def three_sum(self, numbers, target):
         numbers.sort()
-        res = set()
+        res = []
         for i in range(len(numbers) - 2):
+            if i > 0 and numbers[i] == numbers[i - 1]:
+                continue
             j = i + 1
             k = len(numbers) - 1
             while j < k:
-                collection = (numbers[i], numbers[j], numbers[k])
-                if sum(collection) == target:
-                    res.add(collection)
+                triplet = [numbers[i], numbers[j], numbers[k]]
+                total = sum(triplet)
+                if total == target:
+                    res.append(triplet)
+                    while j < k and numbers[j] == numbers[j + 1]:
+                        j += 1
+                    while j < k and numbers[k] == numbers[k - 1]:
+                        k -= 1
                     j += 1
                     k -= 1
-                elif sum(collection) < target:
+                elif total < target:
                     j += 1
                 else:
                     k -= 1
-        return [list(r) for r in res]
+        return res
--- three_sum_test.py
+++ three_sum_test.py
@@ -1,20 +1,38 @@
 from unittest import TestCase
 
+from nose_parameterized import parameterized
+
 from three_sum import Solution
 
 
 class TestThreeSum(TestCase):
-    def test_three_Sum(self):
+    @parameterized.expand([
+        [
+            {
+                'numbers': [-1, 0, 1, 2, -1, 4],
+                'target': 0,
+            },
+            [
+                [-1, -1, 2],
+                [-1, 0, 1],
+            ]
+        ],
+        [
+            {
+                'numbers': [0] * 1000,
+                'target': 0,
+            },
+            [
+                [0, 0, 0],
+            ]
+        ],
+    ])
+    def test_three_Sum(self, kwargs, expected_ans):
         # Setup
         sol = Solution()
-        numbers = [-1, 0, 1, 2, -1, 4]
-        target = 0
 
         # Exercise
-        ans = sol.three_sum(numbers, target)
+        ans = sol.three_sum(**kwargs)
 
         # Verify
-        self.assertEqual(
-            ans,
-            [[-1, -1, 2], [-1, 0, 1]]
-        )
+        self.assertEqual(ans, expected_ans)
