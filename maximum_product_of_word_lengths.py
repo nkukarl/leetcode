87b75ce7a4318c781a8f93bf89e3f5d38d939404
Kai Wang <kai.wang.nankai@gmail.com>
2017-05-15 22:48:26 +1000

    Updated maximum_product_of_word_lengths question

--- maximum_product_of_word_lengths.py
+++ maximum_product_of_word_lengths.py
@@ -18,6 +18,40 @@ class Solution(object):
         return max_product
 
     def summarise(self, word):
+        """
+        Summarise word and return an integer, representing
+        the existence of each character in word.
+
+        For example, word = 'apple'
+
+        num = 0
+
+        ord('a') - ord('a') = 0, num += 2 ** 0
+        ord('p') - ord('a') = 15, num += 2 ** 15
+        ord('l') - ord('a') = 11, num += 2 ** 11
+        ord('e') - ord('a') = 4, num += 2 ** 4
+
+        num = 34833
+
+        Args:
+            word:
+
+        Returns:
+
+        """
+        word = set(word)
+        num = 0
+        for w in word:
+            num += 1 << (ord(w) - ord('a'))
+        return num
+
+        # One-liner
+        # return sum([1 << (ord(w) - ord('a')) for w in set(word)])
+
+    def summarise_(self, word):
+        """
+        This method would be more efficient when word is long.
+        """
         word = set(list(word))
         num = 0
         weight = 1
