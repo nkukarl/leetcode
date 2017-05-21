e1a63efe037f6439c406ca69435745e7a6c9c5a9
Kai Wang <kai.wang.nankai@gmail.com>
2017-05-21 16:01:52 +1000

    Updated integer_replacement question

--- integer_replacement.py
+++ integer_replacement.py
@@ -1,14 +1,30 @@
 class Solution(object):
     def integer_replacement(self, n):
-        self.DICT = {}
-        return self.operate(n)
+        """
+        
+        If n is an even number, let n = 2k, we have
+            f(2k) = f(k) + 1
+        If n is an odd number, let n = 2k + 1, we have
+            f(2k+1) = min[f(2k), f(2k+2)] + 1
+        We then need to deal with two scenarios:
+        <1> n % 4 = 3
+        <2> n % 4 = 1
+            If n % 4 == 3,
+                It is easy to prove that when n = 3,
+                3 -> 2 -> 1 is quicker than 3 -> 4 -> 2 -> 1.
+                Hence, the operation shall be n -= 1
+                
+                TODO: Proof to be added
 
-    def operate(self, n):
-        if 1 == n:
-            return 0
-        if n not in self.DICT:
+        """
+        count = 0
+        while n > 1:
+            count += 1
             if 0 == n % 2:
-                self.DICT[n] = 1 + self.operate(n // 2)
+                n //= 2
             else:
-                self.DICT[n] = 1 + min(self.operate(n + 1), self.operate(n - 1))
-        return self.DICT[n]
+                if 3 == n % 4 and n != 3:
+                    n += 1
+                else:
+                    n -= 1
+        return count
--- integer_replacement_test.py
+++ integer_replacement_test.py
@@ -1,37 +1,33 @@
+import random
 from unittest import TestCase
-
-from nose_parameterized import parameterized
 
 from integer_replacement import Solution
 
 
 class TestIntegerReplacement(TestCase):
-    @parameterized.expand([
-        [
-            {
-                'n': 7,
-            },
-            4,
-        ],
-        [
-            {
-                'n': 8,
-            },
-            3,
-        ],
-        [
-            {
-                'n': 4998695,
-            },
-            30,
-        ]
-    ])
-    def test_integer_replacement(self, kwargs, expected_ans):
+    def test_integer_replacement(self):
         # Setup
         sol = Solution()
+        n = random.randint(1, 2 ** 31 - 1)
 
         # Exercise
-        ans = sol.integer_replacement(**kwargs)
+        ans = sol.integer_replacement(n)
 
         # Verify
+        expected_ans = self.integer_replacement(n)
         self.assertEqual(ans, expected_ans)
+
+    def integer_replacement(self, n):
+        self.DICT = {}
+        return self._operate(n)
+
+    def _operate(self, n):
+        if 1 == n:
+            return 0
+        if n not in self.DICT:
+            if 0 == n % 2:
+                self.DICT[n] = 1 + self._operate(n // 2)
+            else:
+                self.DICT[n] = \
+                    1 + min(self._operate(n + 1), self._operate(n - 1))
+        return self.DICT[n]
