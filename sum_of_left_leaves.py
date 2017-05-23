44dab421fd775bdb39afd15d716a3b3befe8a8f0
Kai Wang <kai.wang.nankai@gmail.com>
2017-05-24 08:00:03 +1000

    Added sum_of_left_leaves question

---
+++ sum_of_left_leaves.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sum_of_left_leaves(self, root):
+        self.total = 0
+        self.explore(root, False)
+        return self.total
+
+    def explore(self, root, is_left):
+        if root is None:
+            return
+        if root.left is None and root.right is None:
+            if is_left:
+                self.total += root.val
+        self.explore(root.left, True)
+        self.explore(root.right, False)
