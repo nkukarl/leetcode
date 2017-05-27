9841f0702b7dbbd380753eb3c2744de1a2bd1e97
Kai Wang <kai.wang.nankai@gmail.com>
2017-05-27 17:23:28 +1000

    Updated intersection_of_two_arrays_ii question

--- intersection_of_two_arrays_ii.py
+++ intersection_of_two_arrays_ii.py
@@ -1,3 +1,10 @@
+# TODO: Follow up question
+# 1. What if the given array is already sorted?
+#    How would you optimize your algorithm?
+# 2. What if nums1's size is small compared to nums2's size?
+#    Which algorithm is better?
+# 3. What if elements of nums2 are stored on disk, and the memory is limited
+#    such that you cannot load all elements into the memory at once?
 class Solution(object):
     def intersect(self, nums1, nums2):
         summary1, summary2 = self.summarise(nums1), self.summarise(nums2)
