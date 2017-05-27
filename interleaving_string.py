a14b11d24ddd321101160e69c576bea2a05b8190
Kai Wang <kai.wang.nankai@gmail.com>
2017-05-27 11:40:00 +1000

    Updated interleaving_string question

--- interleaving_string.py
+++ interleaving_string.py
@@ -1,21 +1,17 @@
-# TODO: Anything more efficient?
 class Solution(object):
     def is_interleave(self, s1, s2, s3):
         len_1, len_2, len_3 = len(s1), len(s2), len(s3)
         if len_1 + len_2 != len_3:
             return False
-        self.s1, self.s2, self.s3 = s1[::-1], s2[::-1], s3[::-1]
-        return self.explore(len_1 - 1, len_2 - 1, len_3 - 1)
-
-    def explore(self, pos_1, pos_2, pos_3):
-        if pos_1 < 0:
-            return self.s2[:pos_2 + 1] == self.s3[:pos_3 + 1]
-        if pos_2 < 0:
-            return self.s1[:pos_1 + 1] == self.s3[:pos_3 + 1]
-
-        flag_1 = flag_2 = False
-        if self.s1[pos_1] == self.s3[pos_3]:
-            flag_1 = self.explore(pos_1 - 1, pos_2, pos_3 - 1)
-        if self.s2[pos_2] == self.s3[pos_3]:
-            flag_2 = self.explore(pos_1, pos_2 - 1, pos_3 - 1)
-        return flag_1 or flag_2
+        last = {(0, 0)}
+        for char in s3:
+            cur = set()
+            for i, j in last:
+                if i < len_1 and s1[i] == char:
+                    cur.add((i + 1, j))
+                if j < len_2 and s2[j] == char:
+                    cur.add((i, j + 1))
+            if not cur:
+                return False
+            last = cur
+        return True
--- interleaving_string_test.py
+++ interleaving_string_test.py
@@ -13,7 +13,6 @@ class TestInterleavingString(TestCase):
                 's2': '',
                 's3': '',
             },
-            True,
         ],
         [
             {
@@ -21,7 +20,6 @@ class TestInterleavingString(TestCase):
                 's2': '',
                 's3': 'a',
             },
-            True,
         ],
         [
             {
@@ -29,7 +27,6 @@ class TestInterleavingString(TestCase):
                 's2': 'abc',
                 's3': 'aabbcc',
             },
-            True,
         ],
         [
             {
@@ -37,7 +34,6 @@ class TestInterleavingString(TestCase):
                 's2': 'dbbca',
                 's3': 'aadbbcbcac',
             },
-            True,
         ],
         [
             {
@@ -45,10 +41,17 @@ class TestInterleavingString(TestCase):
                 's2': 'dbbca',
                 's3': 'aadbbbaccc',
             },
-            False,
+        ],
+        [
+            {
+                's1': 'abbbbbbcabbacaacccababaabcccabcacbcaabbbacccaaaaaababbbacbb',
+                's2': 'ccaacabbacaccacababbbbabbcacccacccccaabaababacbbacabbbbabc',
+                's3': 'cacbabbacbbbabcbaacbbaccacaacaacccabababbbababcccbabcabb'
+                      'accabcccacccaabbcbcaccccaaaaabaaaaababbbbacbbabacbbacabbbbabc',
+            },
         ],
     ])
-    def test_is_interleave(self, kwargs, expected_ans):
+    def test_is_interleave(self, kwargs):
         # Setup
         sol = Solution()
 
@@ -56,4 +59,25 @@ class TestInterleavingString(TestCase):
         ans = sol.is_interleave(**kwargs)
 
         # Verify
+        expected_ans = self.is_interleave(**kwargs)
         self.assertEqual(ans, expected_ans)
+
+    def is_interleave(self, s1, s2, s3):
+        len_1, len_2, len_3 = len(s1), len(s2), len(s3)
+        if len_1 + len_2 != len_3:
+            return False
+        self.s1, self.s2, self.s3 = s1[::-1], s2[::-1], s3[::-1]
+        return self.explore(len_1 - 1, len_2 - 1, len_3 - 1)
+
+    def explore(self, pos_1, pos_2, pos_3):
+        if pos_1 < 0:
+            return self.s2[:pos_2 + 1] == self.s3[:pos_3 + 1]
+        if pos_2 < 0:
+            return self.s1[:pos_1 + 1] == self.s3[:pos_3 + 1]
+
+        flag_1 = flag_2 = False
+        if self.s1[pos_1] == self.s3[pos_3]:
+            flag_1 = self.explore(pos_1 - 1, pos_2, pos_3 - 1)
+        if self.s2[pos_2] == self.s3[pos_3]:
+            flag_2 = self.explore(pos_1, pos_2 - 1, pos_3 - 1)
+        return flag_1 or flag_2
