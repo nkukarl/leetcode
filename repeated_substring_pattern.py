f0f3b5c0ccd081e387ca2bfe5714665937b1a728
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-12 00:02:15 +1000

    Updated repeated_substring_pattern question

--- repeated_substring_pattern.py
+++ repeated_substring_pattern.py
@@ -1,27 +1,12 @@
 class Solution(object):
     def repeated_substring_pattern(self, s):
-        # Handle simple scenario
-        if len(s) < 2:
-            return False
-        if 2 == len(s):
-            return s[0] == s[1]
-
-        pattern = s[0]
-        half_s = s[:len(s) // 2 + 1]
-        for char in half_s[1:]:
-            if char == s[0]:
-                if self.check_pattern(s, pattern):
+        for i in range(1, len(s) // 2 + 1):
+            # This implicitly confirms len(s) > 0
+            if s[i] == s[0]:
+                pattern = s[:i]
+                if len(s) % i != 0:
+                    continue
+                if s.split(pattern) == [''] * (len(s) // i + 1):
                     return True
-            pattern += char
-        return False
 
-    def check_pattern(self, s, pattern):
-        m, n = len(s), len(pattern)
-        if m % n != 0:
-            return False
-        k = m // n
-        for i in range(k):
-            cur = s[i * len(pattern): (i + 1) * len(pattern)]
-            if cur != pattern:
-                return False
-        return True
+        return False
--- repeated_substring_pattern_test.py
+++ repeated_substring_pattern_test.py
@@ -9,30 +9,37 @@ class TestRepeatedSubstringPattern(TestCase):
     @parameterized.expand([
         [
             {
+                's': '',
+            },
+        ],
+        [
+            {
+                's': 'a',
+            },
+        ],
+        [
+            {
                 's': 'abcabcabc',
             },
-            True,
         ],
         [
             {
                 's': 'abababab',
             },
-            True,
         ],
         [
             {
                 's': 'abcabcabd',
             },
-            False,
         ],
         [
             {
                 's': 'aba',
             },
-            False
         ],
+
     ])
-    def test_repeated_substring_pattern(self, kwargs, expected_ans):
+    def test_repeated_substring_pattern(self, kwargs):
         # Setup
         sol = Solution()
 
@@ -40,4 +47,8 @@ class TestRepeatedSubstringPattern(TestCase):
         ans = sol.repeated_substring_pattern(**kwargs)
 
         # Verify
+        expected_ans = self.repeated_substring_pattern(**kwargs)
         self.assertEqual(ans, expected_ans)
+
+    def repeated_substring_pattern(self, s):
+        return s != '' and s in (s * 2)[1:-1]
