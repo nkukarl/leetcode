3b141d42546dee158576e76db3bdc048216aa06a
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-11 17:31:57 +1000

    Updated add_two_numbers_ii question

--- add_two_numbers_ii.py
+++ add_two_numbers_ii.py
@@ -3,31 +3,75 @@ from utils_linked_list import ListNode
 
 class Solution(object):
     def add_two_numbers(self, head1, head2):
-        stack1 = self.get_stack(head1)
-        stack2 = self.get_stack(head2)
-        stack = []
-        carry = 0
-        while len(stack1) > 0 or len(stack2) > 0:
-            val1 = stack1.pop() if len(stack1) > 0 else 0
-            val2 = stack2.pop() if len(stack2) > 0 else 0
-            res = val1 + val2 + carry
-            val = res % 10
-            carry = res // 10
-            stack.append(val)
+        l1, l2 = self.get_length(head1), self.get_length(head2)
+        if l1 == l2:
+            head_raw, carry = self.add_two_lists(head1, head2)
+            if 1 == carry:
+                head = ListNode(1)
+                head.next = head_raw
+            else:
+                head = head_raw
+            return head
+
+        if l1 < l2:
+            head1, head2 = head2, head1
+
+        prev_ = None
+        node_ = head1
+        diff = abs(l1 - l2)
+        while diff > 0:
+            prev_ = node_
+            node_ = node_.next
+            diff -= 1
+
+        # Split the first list into two lists
+        node, carry = self.add_two_lists(node_, head2)
         if 1 == carry:
-            stack.append(1)
+            head_raw, carry_, last = self.add_one_to_a_list(head1, prev_)
+            if 1 == carry_:
+                head = ListNode(1)
+                head.next = head_raw
+            else:
+                head = head_raw
+        else:
+            head, last = self.copy_a_list(head1, prev_)
+        # Relink the two lists separated from the first list
+        last.next = node
+        return head
 
-        dummy = ListNode(0)
-        node = dummy
-        while len(stack) > 0:
-            node.next = ListNode(stack.pop())
-            node = node.next
-        return dummy.next
+    def add_two_lists(self, head1, head2):
+        if head1 is None and head2 is None:
+            return None, 0
+        next_, carry = self.add_two_lists(head1.next, head2.next)
+        sum_ = carry + head1.val + head2.val
+        node = ListNode(sum_ % 10)
+        node.next = next_
+        return node, sum_ // 10
 
-    def get_stack(self, head):
-        stack = []
-        node = head
-        while node is not None:
-            stack.append(node.val)
-            node = node.next
-        return stack
+    def add_one_to_a_list(self, head, tail):
+        if head == tail.next:
+            return None, 1, None
+        next_, carry, last = self.add_one_to_a_list(head.next, tail)
+        sum_ = carry + head.val
+        node = ListNode(sum_ % 10)
+        node.next = next_
+        if last is None:
+            return node, sum_ // 10, node
+        return node, sum_ // 10, last
+
+    def copy_a_list(self, head, tail):
+        if head == tail.next:
+            return None, None
+        next_, last = self.copy_a_list(head.next, tail)
+        node = ListNode(head.val)
+        node.next = next_
+        if last is None:
+            return node, node
+        return node, last
+
+    def get_length(self, head):
+        count = 0
+        while head is not None:
+            count += 1
+            head = head.next
+        return count
