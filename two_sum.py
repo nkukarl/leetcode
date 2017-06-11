53eb5e16f04c1fb871402b3bf96f16817f718764
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-11 12:18:05 +1000

    Updated two_sum question

--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,19 @@
 class Solution:
-    def two_sum(self, numbers, target):
+    def two_sum(self, nums, target):
         cache = dict()
-        for i in range(len(numbers)):
-            new_target = target - numbers[i]
+        for i in range(len(nums)):
+            new_target = target - nums[i]
             if new_target in cache:
-                return [cache[new_target], i]
-            cache[numbers[i]] = i
+                return cache[new_target], i
+            cache[nums[i]] = i
+
+    def two_sum_sorted_nums(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left < right:
+            sum_ = nums[left] + nums[right]
+            if sum_ == target:
+                return left, right
+            if sum_ < target:
+                left += 1
+            else:
+                right -= 1
--- two_sum_test.py
+++ two_sum_test.py
@@ -4,14 +4,26 @@ from two_sum import Solution
 
 
 class TestTwoSum(TestCase):
-    def test_twoSum(self):
+    def test_two_sum(self):
         # Setup
         sol = Solution()
-        numbers = [2, 7, 11, 15]
+        nums = [2, 7, 11, 15]
         target = 17
 
         # Exercise
-        ans = sol.two_sum(numbers, target)
+        ans = sol.two_sum(nums, target)
 
         # Verify
-        self.assertEqual(ans, [0, 3])
+        self.assertEqual(ans, (0, 3))
+
+    def test_two_sum_sorted_nums(self):
+        # Setup
+        sol = Solution()
+        nums = [2, 7, 11, 15]
+        target = 9
+
+        # Exercise
+        ans = sol.two_sum_sorted_nums(nums, target)
+
+        # Verify
+        self.assertEqual(ans, (0, 1))
