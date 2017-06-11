6c52ca6aeb0be12235bcd01c11dc193fdec29915
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-11 10:49:41 +1000

    Updated find_the_difference question

--- find_the_difference.py
+++ find_the_difference.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def find_the_difference(self, s, t):
-        summary_s = self.summarise(s)
-        summary_t = self.summarise(t)
-        for c, n in summary_t.items():
-            if n != summary_s.get(c, 0):
-                return c
-
-    def summarise(self, s):
         summary = {}
-        for c in s:
-            summary[c] = summary.get(c, 0) + 1
-        return summary
+        for char in s:
+            summary[char] = summary.get(char, 0) + 1
+        for char in t:
+            if 0 == summary.get(char, 0):
+                return char
+            summary[char] -= 1
+
+    def find_the_difference_xor(self, s, t):
+        xor = 0
+        for char in s + t:
+            xor ^= ord(char)
+        return chr(xor)
--- find_the_difference_test.py
+++ find_the_difference_test.py
@@ -20,6 +20,21 @@ class TestFindTheDifference(TestCase):
 
         # Exercise
         ans = sol.find_the_difference(s, t)
+        ans_xor = sol.find_the_difference_xor(s, t)
 
         # Verify
         self.assertEqual(ans, ex)
+        self.assertEqual(ans_xor, ex)
+
+    def find_the_difference(self, s, t):
+        summary_s = self.summarise(s)
+        summary_t = self.summarise(t)
+        for c, n in summary_t.items():
+            if n != summary_s.get(c, 0):
+                return c
+
+    def summarise(self, s):
+        summary = {}
+        for c in s:
+            summary[c] = summary.get(c, 0) + 1
+        return summary
