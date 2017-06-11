6e84c71a6f3ce237ff7d049a26b49a4153dbc52e
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-11 15:28:48 +1000

    Updated binary_tree_tilt question

--- binary_tree_tilt.py
+++ binary_tree_tilt.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def find_tilt(self, root):
-        self.tilt = 0
-        self.get_sum(root)
-        return self.tilt
+        return self.get_tilt_and_sum(root)[0]
 
-    def get_sum(self, root):
+    def get_tilt_and_sum(self, root):
         if root is None:
-            return 0
-        sum_left = self.get_sum(root.left)
-        sum_right = self.get_sum(root.right)
-        self.tilt += abs(sum_left - sum_right)
-        return root.val + sum_left + sum_right
+            return 0, 0
+        left_tilt, left_sum = self.get_tilt_and_sum(root.left)
+        right_tilt, right_sum = self.get_tilt_and_sum(root.right)
+        current_tilt = abs(left_sum - right_sum)
+
+        total_tilt = left_tilt + right_tilt + current_tilt
+        total_sum = left_sum + right_sum + root.val
+        return total_tilt, total_sum
--- binary_tree_tilt_test.py
+++ binary_tree_tilt_test.py
@@ -1,19 +1,33 @@
 from unittest import TestCase
 
+from nose_parameterized import parameterized
+
 from binary_tree_tilt import Solution
-from utils_tree import get_root_tree
+from utils_tree import construct_tree
 
 
 class TestBinaryTreeTilt(TestCase):
-    def test_find_tilt(self):
+    @parameterized.expand([
+        [
+            [
+                [2], [1, 3],
+            ],
+            2,
+        ],
+        [
+            [
+                [1], [2], [3, 4], [5, None, 6, 7], [None, 9],
+            ],
+            60,
+        ]
+    ])
+    def test_find_tilt(self, serialized_data, expected_ans):
         # Setup
         sol = Solution()
-        tree_raw = [1, 2, 3]
-        root = get_root_tree(tree_raw)
+        root = construct_tree(serialized_data)
 
         # Exercise
         ans = sol.find_tilt(root)
 
         # Verify
-        expected_ans = 2
         self.assertEqual(ans, expected_ans)
