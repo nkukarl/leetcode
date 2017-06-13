3cc408fc642d8c94963c8631294390f1b575973a
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-13 20:52:52 +1000

    Updated evaluate_reverse_polish_notation question

--- evaluate_reverse_polish_notation.py
+++ evaluate_reverse_polish_notation.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def eval_rpn(self, tokens):
         stack = []
-        operator = {
+        ops = {
             '+': lambda x, y: x + y,
             '-': lambda x, y: x - y,
             '*': lambda x, y: x * y,
             '/': lambda x, y: abs(x) // abs(y) * (1 if x * y > 0 else -1),
         }
         for t in tokens:
-            if t in operator:
-                second = stack.pop()
-                first = stack.pop()
-                f = operator[t]
-                stack.append(f(first, second))
+            if t in ops:
+                y, x = stack.pop(), stack.pop()
+                stack.append(ops[t](x, y))
             else:
                 stack.append(int(t))
         return stack[0] if stack != [] else 0
