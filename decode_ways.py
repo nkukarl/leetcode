d8adc93ecd36dba1882c96130e5f816ec6123491
Kai Wang <kai.wang.nankai@gmail.com>
2017-06-15 23:26:05 +1000

    Updated decode_ways question

--- decode_ways.py
+++ decode_ways.py
@@ -1,20 +1,34 @@
 class Solution(object):
     def num_decodings(self, s):
-        if '' == s or s.startswith('0'):
+        """
+        Some examples to approach this problem:
+        
+        s = '0XXX'
+        
+        s = 'XXX10'
+        s = 'XXX20'
+        s = 'XXX40'
+        
+        s = 'XXX1?'
+        s = 'XXX2?'
+        s = 'XXX3?'
+        
+        """
+        if not s or s.startswith('0'):
             return 0
-        counts = [1, 1]
+        count_1, count_2 = 1, 1
         for i in range(1, len(s)):
             if '0' == s[i]:
                 if s[i - 1] not in '12':
                     return 0
-                counts.append(counts[-2])
+                count_1, count_2 = count_2, count_1
             elif s[i - 1] == '1':
-                counts.append(counts[-1] + counts[-2])
+                count_1, count_2 = count_2, count_1 + count_2
             elif s[i - 1] == '2':
                 if s[i] in '123456':
-                    counts.append(counts[-1] + counts[-2])
+                    count_1, count_2 = count_2, count_1 + count_2
                 else:
-                    counts.append(counts[-1])
+                    count_1 = count_2
             else:
-                counts.append(counts[-1])
-        return counts[-1]
+                count_1 = count_2
+        return count_2
